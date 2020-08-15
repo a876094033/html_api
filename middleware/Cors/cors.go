61dbde68e108a6a4a70533e232d25fb295e98173
Your Name <you@example.com>
2020-08-15 15:56:08 +0800

    test

--- middleware/Cors/cors.go
+++ middleware/Cors/cors.go
@@ -9,11 +9,12 @@ func Cors() gin.HandlerFunc {
 		method := c.Request.Method
 		//_ := c.Request.Header.Get("Origin")
 		//if origin != "" {
-			c.Header("Access-Control-Allow-Origin", "*")
+			c.Header("Access-Control-Allow-Origin", "http://www.samsung-fund.com")
 			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
 			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
 			c.Header("Access-Control-Allow-Credentials", "true")
+
 			c.Set("content-type", "application/json")
 		//}
 		if method == "OPTIONS" {
