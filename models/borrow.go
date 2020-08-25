433458a6b443d25cb5ae83303ac5782c207dfa2a
Your Name <you@example.com>
2020-08-25 17:18:52 +0800

    test

--- models/borrow.go
+++ models/borrow.go
@@ -3,20 +3,22 @@ package models
 import "github.com/jinzhu/gorm"
 
 type Borrow struct {
-	ID           int     `json:"id" gorm:"primary_key"`
-	BorrowName   string  `json:"borrow_name"`
-	InterestRate float64 `json:"interest_rate"`
-	Term         int     `json:"term"`
-	TermType     int     `json:"term_type"`
-	Amount       float64 `json:"amount"`
-	RepayType    int     `json:"repay_type"`
-	Purpose      string  `json:"purpose"`
-	RepayName    string  `json:"repay_name"`
-	Diya         string  `json:"diya"`
-	AmountLimit  float64 `json:"amount_limit"`
-	BorrowStatus int     `json:"borrow_status"`
-	Ramerk       string  `json:"ramerk"`
-	BorrowImg    string  `json:"borrow_img"`
+	ID              int     `json:"id" gorm:"primary_key"`
+	BorrowName      string  `json:"borrow_name"`
+	InterestRate    float64 `json:"interest_rate"`
+	Term            int     `json:"term"`
+	TermType        int     `json:"term_type"`
+	Amount          float64 `json:"amount"`
+	RepayType       int     `json:"repay_type"`
+	Purpose         string  `json:"purpose"`
+	RepayName       string  `json:"repay_name"`
+	Diya            string  `json:"diya"`
+	AmountLimit     float64 `json:"amount_limit"`
+	BorrowStatus    int     `json:"borrow_status"`
+	Ramerk          string  `json:"ramerk"`
+	BorrowImg       string  `json:"borrow_img"`
+	BorrowStructImg string  `json:"borrow_struct_img"`
+	ProvePdf        string  `json:"prove_pdf"`
 }
 type BorrowAll struct {
 	Borrow
@@ -26,7 +28,7 @@ type BorrowAll struct {
 
 func GetBorrows(pageNum int, pageSize int, maps interface{}) ([]*Borrow, error) {
 	var borrows []*Borrow
-	err := db.Debug().Model(&Borrow{}).Where(maps).Where("borrow_status<>?", 1).Offset(pageNum/10*pageSize).Limit(pageSize).Find(&borrows).Error
+	err := db.Debug().Model(&Borrow{}).Where(maps).Where("borrow_status<>?", 1).Offset(pageNum / 10 * pageSize).Limit(pageSize).Find(&borrows).Error
 	if err != nil && err != gorm.ErrRecordNotFound {
 		return nil, err
 	}
